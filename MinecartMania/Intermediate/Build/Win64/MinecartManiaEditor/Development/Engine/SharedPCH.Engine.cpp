cf2c11372ce8b9b5971bc0c688770dac339726dd
Hayden Matthews <haydenamatthews@yahoo.com>
2018-12-05 16:27:43 -0700

    Tightened up controls and fixed a few bugs.

--- MinecartMania/Intermediate/Build/Win64/MinecartManiaEditor/Development/Engine/SharedPCH.Engine.cpp
+++ MinecartMania/Intermediate/Build/Win64/MinecartManiaEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Hayden/Documents/Minecart-Mania/MinecartMania/Intermediate/Build/Win64/MinecartManiaEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/hayde/Documents/Minecart-Mania/MinecartMania/Intermediate/Build/Win64/MinecartManiaEditor/Development/Engine/SharedPCH.Engine.h"
